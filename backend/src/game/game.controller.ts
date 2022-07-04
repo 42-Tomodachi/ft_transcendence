5567e02eb650b58e82117293b1ef04463302b776
usanglee <mail@usanglee.com>
2022-07-04 22:04:34 +0900

    Fix: game controller API에 체크표시 추가

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -9,13 +9,13 @@ import { GameService } from './game.service';
 export class GameController {
     constructor(private gameService: GameService) { }
 
-    @ApiOperation({ summary: 'ulee 게임방 목록 가져오기' })
+    @ApiOperation({ summary: 'ulee✅ 게임방 목록 가져오기' })
     @Get('/')
     getGameRooms(): GetGameRoomsDto[] {
         return this.gameService.getGameRooms();
     }
 
-    @ApiOperation({ summary: 'ulee 게임방 참여자(플레이어) 정보 가져오기' })
+    @ApiOperation({ summary: 'ulee✅ 게임방 참여자(플레이어) 정보 가져오기' })
     @Get('/:gameId/users')
     async getGameUsers(
         @Param('gameId', ParseIntPipe) gameId: number,
@@ -23,7 +23,7 @@ export class GameController {
         return this.gameService.getGameUsers(gameId);
     }
 
-    @ApiOperation({ summary: 'ulee 게임방 만들기' })
+    @ApiOperation({ summary: 'ulee✅ 게임방 만들기' })
     @Post('/')
     createGameRoom(
         @Body() createGameRoomDto: CreateGameRoomDto
@@ -31,7 +31,7 @@ export class GameController {
         return this.gameService.createGameRoom(createGameRoomDto);
     }
 
-    @ApiOperation({ summary: 'ulee 게임방 입장하기' })
+    @ApiOperation({ summary: 'ulee✅ 게임방 입장하기' })
     @Post('/:gameId/users/:userId')
     enterGameRoom(
         @Param('gameId', ParseIntPipe) gameId: number,
@@ -40,7 +40,7 @@ export class GameController {
         return this.gameService.enterGameRoom(gameId, userId);
     }
 
-    @ApiOperation({ summary: 'ulee 게임방 퇴장하기' })
+    @ApiOperation({ summary: 'ulee✅ 게임방 퇴장하기' })
     @Delete('/:gameId/users/:userId')
     exitGameRoom(
         @Param('gameId', ParseIntPipe) gameId: number,
