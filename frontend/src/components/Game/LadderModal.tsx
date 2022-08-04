2bf3a80ccb6969db5b2a7deac42d57b1de2a963a
junselee42 <junselee@student.42seoul.kr>
2022-08-04 20:28:25 +0900

    CHORE: tested  modalcancel.. with background click

--- frontend/src/components/Game/LadderModal.tsx
+++ frontend/src/components/Game/LadderModal.tsx
@@ -16,11 +16,6 @@ const LadderModal: React.FC = () => {
   useEffect(() => {
     console.log('modal!!!!!!!!!');
     socket = io(`${process.env.REACT_APP_BACK_API}`, {
-      //REACT_APP_BACK_API_B
-      //socket = io('http://localhost:5500/', {
-      // socket = io('http://10.19.210.94:5500/', {
-      // socket = io('process.env.REACT_APP_BACK_API_A', {
-      //socket = io('http://3.34.203.69:5500/', {
       transports: ['websocket'], // 웹소켓으로 간다는걸 알려준다. 구글링.
       query: {
         userId: user?.userId,
@@ -43,6 +38,9 @@ const LadderModal: React.FC = () => {
       if (user) user.roomid = roomId;
       navigate(`/gameroom/`); //GamePage.tsx
     });
+    return () => {
+      socket.emit('cancelLadderQueue');
+    };
   }, []);
   return (
     <Modal width={400} height={200}>
