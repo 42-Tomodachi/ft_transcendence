2d12b0f0d0766bba3248b14b57cee844b2d5c532
junselee42 <junselee@student.42seoul.kr>
2022-09-03 02:55:47 +0900

    [FE] FIX: 소켓이벤트 'playerDisconnected' 발생시 동작 수정 && 일부 로직 함수화

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -8,7 +8,7 @@ import ProfileImage from '../components/common/ProfileImage';
 import { io, Socket } from 'socket.io-client'; // 아이오 연결하고.
 import GameStart from './GameStart';
 import { useNavigate } from 'react-router-dom'; //LoadingPage
-import { SHOW_PROFILE } from '../utils/interface';
+import LoadingPage from './LoadingPage';
 
 interface GameInfoDto {
   // 내가 쓸 변수
@@ -32,33 +32,17 @@ interface GameInfoDto {
   ladderLevel: number;
 }
 
-// interface UserProfileDto {
-//   userId: number;
-//   nickname: string;
-//   avatar: string | null;
-//   email: string;
-//   winCount: number;
-//   loseCount: number;
-//   ladderWinCount: number;
-//   ladderLoseCount: number;
-//   ladderLevel: number;
-// }
-
 /*
  * 모달 페이지로 큐에 게임매칭을 수행하게 되면, 이 페이지로 이동합니다.
  * 매칭이 이루어졌다면, 서버에서 매칭유저에 대한 정보를 보내주기로 합의되어있다.
  */
 const GamePage: React.FC = () => {
-  console.log('re-render?\n');
-
-  const { setModal } = useContext(AllContext).modalData;
   let socket: Socket;
   const navigate = useNavigate();
   const [gameStart, setGameStart] = useState(false);
   const { user } = useContext(AllContext).userData; // setUser
-  // const { setUserStatus } = useContext(AllContext).userStatus;
   const { playingGameInfo, setPlayingGameInfo } = useContext(AllContext).playingGameInfo;
-  const [count, setCount] = useState(5); //오피셜은 10초
+  const [count, setCount] = useState<number>(); //오피셜은 10초
   const [info, setInfo] = useState<GameInfoDto>({
     nicknameOne: '',
     avatarOne: '',
@@ -79,92 +63,128 @@ const GamePage: React.FC = () => {
     ladderLevel: 0,
   });
 
-  //console.log(user); // null
-  //console.log('게임:' + gameStart); // false
+  const settingMatchData = (p1: GameInfoDto, p2: GameInfoDto) => {
+    if (p1 && p2 && user) {
+      if (user.nickname === p1.nickname)
+        setPlayingGameInfo({
+          ...playingGameInfo,
+          player: 'p1',
+          oneNickname: p1.nickname,
+          twoNickname: p2.nickname,
+        });
+      else if (user.nickname === p2.nickname)
+        setPlayingGameInfo({
+          ...playingGameInfo,
+          player: 'p2',
+          oneNickname: p1.nickname,
+          twoNickname: p2.nickname,
+        });
+      else {
+        setPlayingGameInfo({
+          ...playingGameInfo,
+          player: 'g1',
+          oneNickname: p1.nickname,
+          twoNickname: p2.nickname,
+        });
+      }
+    }
+  };
+
+  const socketConnect = (roomid: number) => {
+    socket = io(`${process.env.REACT_APP_BACK_API}/ws-game`, {
+      transports: ['websocket'],
+      multiplex: false,
+      query: {
+        userId: user && user.userId,
+        roomId: roomid && roomid,
+        connectionType: playingGameInfo.gameLadder ? 'ladderGame' : 'normalGame',
+      },
+    });
+
+    socket.on('message', () => {
+      console.log(` 일반 connected socket : ${socket.id}`);
+      console.log(socket.connected);
+      if (user) user.socket = socket;
+    });
+  };
+
+  const socketDisonnect = (socket: Socket) => {
+    if (socket) {
+      socket.off('playerDisconnected');
+      socket.off('gameDestroyed');
+      socket.off('gameStartCount');
+      socket.off('matchData');
+      socket.disconnect();
+    }
+  };
 
   useEffect(() => {
     const win: HTMLElement | null = document.getElementById('win');
     const win2: HTMLElement | null = document.getElementById('win2');
-    const roomid: number = playingGameInfo.gameRoomId;
+    const roomId: number = playingGameInfo.gameRoomId;
 
     if (user && playingGameInfo) {
-      //이제 무조건 소켓을 연결할겁니다. 커넥션타입을 어떻게 분기해줄것인지.. 생각해봅시다.
-      //gameMode가 아니고, 모달에서 ladder인걸 기록해놓는 무언가 필요한것입니다아...
-      //socket = io(`${process.env.REACT_APP_BACK_API}/ws-game`
-      socket = io(`${process.env.REACT_APP_BACK_API}/ws-game`, {
-        transports: ['websocket'],
-        multiplex: false,
-        query: {
-          userId: user && user.userId,
-          roomId: roomid && roomid,
-          connectionType: playingGameInfo.gameLadder ? 'ladderGame' : 'normalGame',
-        },
-      }); // roomid 유저상태
-      socket.on('message', () => {
-        console.log(` 일반 connected socket : ${socket.id}`);
-        console.log(socket.connected);
-        if (user) user.socket = socket;
-      });
-      socket.emit('onMatchingScreen', roomid);
-      // 래더든 일반이든 지금 데이터가 필요하다는걸 서버한테 알려주고,
-      // matchData 이벤트로 보내주길..하염없이 기다리는 코드.
-      // gameMode가 아니라 래더인지 아닌지를 알려주는 변수여야함!!
+      socketConnect(roomId);
+
+      /*
+       * 서버에게 매치데이터를 받을 준비가 되었다고 알립니다.
+       * roomid를 넘겨받은 서버는, matchData를 게임방의 유저들에게 보내줍니다.
+       */
+      socket.emit('onMatchingScreen', roomId);
+
+      /*
+       * 래더게임방일 경우를 확인합니다.
+       * playerDisconnected 로 유저 연결상태를 확인합니다.
+       * 표시해줄 매치데이타를 래더에 맞게 가공하고 저장합니다.
+       */
       if (playingGameInfo.gameLadder === true) {
         playingGameInfo.gameLadder = false;
-        socket.on('playerDisconnected', data => {
-          console.log(`playerDisconnected: 누군가 튕겼을때연락옴. (여긴래더) ${data}`);
-          console.log(`playerDisconnected: 튕겼다는건 나가기버튼이 아니고 새로고침임 ${data}`);
-          console.log(`playerDisconnected: 방장이 새로고침으로 튕기면 폭파되게 해야함 ${data}`);
-          console.log(`playerDisconnected: 나가기 버튼으로 나간건 디스트로이이벤트 연락옴 ${data}`);
-          console.log(`playerDisconnected: 근데 래더지금 이 연락안옴 ${data}`);
-          socket.disconnect();
-          navigate('/game');
-        });
-        /////////////////////////////////////////////////////////////////////
         socket.on('matchData', (p1: GameInfoDto, p2: GameInfoDto) => {
-          // console.log('MMMMMMMAttata');
-          setInfo(info => {
-            return {
-              ...info,
-              nicknameOne: p1.nickname,
-              avatarOne: p1.avatar,
-              winCountOne: p1.ladderWinCount,
-              loseCountOne: p1.ladderLoseCount,
-              ladderLevelOne: p1.ladderLevel,
-              nicknameTwo: p2.nickname,
-              avatarTwo: p2.avatar,
-              winCountTwo: p2.ladderWinCount,
-              loseCountTwo: p2.ladderLoseCount,
-              ladderLevelTwo: p2.ladderLevel,
-            };
-          });
-          // 여기부터끝까지가 래더랑 일반이 구조가 중복되니까. 이 리팩이 무사히 돌아가면 함수로 빼겠다는 계획. 1111
-          if (win)
-            win.style.width = `${
-              (p1.ladderWinCount / (p1.ladderWinCount + p1.ladderLoseCount)) * 100
-            }%`;
-          if (win2)
-            win2.style.width = `${
-              (p2.ladderWinCount / (p2.ladderWinCount + p2.ladderLoseCount)) * 100
-            }%`;
-          if (user?.nickname === p1.nickname)
-            setPlayingGameInfo({
-              ...playingGameInfo,
-              player: 'p1',
-              oneNickname: p1.nickname,
-              twoNickname: p2.nickname,
+          if (!p1) {
+            socketDisonnect(socket);
+            navigate('/game');
+          }
+          if (p1 && p2) {
+            setInfo(info => {
+              return {
+                ...info,
+                nicknameOne: p1.nickname,
+                avatarOne: p1.avatar,
+                winCountOne: p1.ladderWinCount,
+                loseCountOne: p1.ladderLoseCount,
+                ladderLevelOne: p1.ladderLevel,
+                nicknameTwo: p2.nickname,
+                avatarTwo: p2.avatar,
+                winCountTwo: p2.ladderWinCount,
+                loseCountTwo: p2.ladderLoseCount,
+                ladderLevelTwo: p2.ladderLevel,
+              };
             });
-          else if (user?.nickname === p2.nickname)
-            setPlayingGameInfo({
-              ...playingGameInfo,
-              player: 'p2',
-              oneNickname: p1.nickname,
-              twoNickname: p2.nickname,
-            });
+            if (win)
+              win.style.width = `${
+                (p1.ladderWinCount / (p1.ladderWinCount + p1.ladderLoseCount)) * 100
+              }%`;
+            if (win2)
+              win2.style.width = `${
+                (p2.ladderWinCount / (p2.ladderWinCount + p2.ladderLoseCount)) * 100
+              }%`;
+            settingMatchData(p1, p2);
+          }
         });
       } else {
+        /*
+         * 이름 : matchData, p1, p2(대전유저들의 프로필정보를 받아옵니다)
+         * 특징 : p2가 게임시작전에 나갈경우, p1과 p2에 모두 null이 담긴 연락이 옵니다.
+         * 사용 : 래더의 경우는 무조건 폭파되는 조건으로 활용하고, 일반게임의 경우 플레이어2의 상태를 갱신합니다.
+         */
         socket.on('matchData', (p1: GameInfoDto, p2: GameInfoDto) => {
+          /*
+           * 화살표함수와 return을 활용하여, 직전데이터를 받아 갱신합니다.
+           * 일반적인 useState함수로 처리할 경우에는, info의 데이터가 최신이 아닙니다(비동기)
+           * 전적바 디폴트로 수정해줍니다.
+           */
           if (!p1 && !p2) {
+            setCount(undefined);
             setInfo(info => {
               return {
                 ...info,
@@ -175,14 +195,6 @@ const GamePage: React.FC = () => {
                 ladderLevelTwo: 0,
               };
             });
-            // setInfo({
-            //   ...info,
-            //   nicknameTwo: '곧 들어와요?!',
-            //   avatarTwo: '',
-            //   winCountTwo: 0,
-            //   loseCountTwo: 0,
-            //   ladderLevelTwo: 0,
-            // });
             if (win2) win2.style.width = `50%`;
           } else {
             setInfo({
@@ -198,139 +210,106 @@ const GamePage: React.FC = () => {
               loseCountTwo: p2 ? p2.loseCount : info.loseCountTwo,
               ladderLevelTwo: p2 ? p2.ladderLevel : info.ladderLevelTwo,
             });
-            // 전적표시 바를 승률에 맞게 커스텀 하는 부분
-            // 여기부터끝까지가 래더랑 일반이 구조가 중복되니까. 이 리팩이 무사히 돌아가면 함수로 빼겠다는 계획. 2222
             if (win) win.style.width = `${(p1.winCount / (p1.winCount + p1.loseCount)) * 100}%`;
             if (win2 && p2)
               win2.style.width = `${(p2.winCount / (p2.winCount + p2.loseCount)) * 100}%`;
-
-            // 플레이어가 p1이거나 p2이거나, g1임 (관전자)
-            if (user?.nickname === p1.nickname)
-              setPlayingGameInfo({
-                ...playingGameInfo,
-                player: 'p1',
-                oneNickname: p1.nickname,
-                twoNickname: p2 ? p2.nickname : '',
-              });
-            else if (p2 && user?.nickname === p2.nickname)
-              setPlayingGameInfo({
-                ...playingGameInfo,
-                player: 'p2',
-                oneNickname: p1.nickname,
-                twoNickname: p2.nickname,
-              });
-            else {
-              setPlayingGameInfo({
-                ...playingGameInfo,
-                player: 'g1',
-                oneNickname: p1.nickname,
-                twoNickname: p2.nickname,
-              });
-            }
+            settingMatchData(p1, p2);
           }
         });
       }
-      //////////////////////////////////// 방폭파 이벤트 공통!!! //////////////////////////////////////
+      /*
+       * 이름 : gameDestroyed, ()
+       * 특징 : 방에 폭파조건에 부합하면 발생됩니다.
+       *       일반게임 시작전, 방장이 나가기 버튼을 누른것을 감지합니다.
+       * 사용 : 방폭파를 위해 남아있는 유저들을 정리합니다.
+       */
       socket.on('gameDestroyed', () => {
-        console.log(`gameDestroyed: 래더, 일반 모두 동작\n`);
-        console.log(`gameDestroyed: 게임시작전 방장이 나가면 폭파했다고 연락옴\n`);
-        console.log(`gameDestroyed: 새로고침 아닌 나가기버튼일때만 연락옴\n`);
-        socket.disconnect();
+        socketDisonnect(socket);
         navigate('/game');
       });
+      /*
+       * 이름 : gameStartCount, data(서버에서 계산한 카운트다운 넘버)
+       * 특징 : 게임시작전 5초를 대기하며, 서버로부터 넘버를 받습니다.
+       * 사용 : 보내온 카운트다운 넘버가  0될때, 게임을 시작합니다.
+       */
       socket.on('gameStartCount', (data: number) => {
-        console.log('countdown:' + data);
         setCount(data);
         if (data == 0) {
           setGameStart(true);
         }
-        //////////////////////////////////// 플레이어 튕김 이벤트!!! //////////////////////////////////////
-        if (playingGameInfo.gameLadder !== false)
-          socket.on('playerDisconnected', data => {
-            console.log(`playerDisconnected: 일반에서 누군가 튕겼을때연락옴. (여긴일반) ${data}`);
-            console.log(`playerDisconnected: 튕겼다는건 나가기버튼이 아니고 새로고침임 ${data}`);
-            console.log(`playerDisconnected: 방장이 새로고침으로 튕기면 폭파되게 해야함 ${data}`);
-            console.log(
-              `playerDisconnected: 나가기 버튼으로 나간건 디스트로이이벤트 연락옴 ${data}`,
-            );
-            console.log(`playerDisconnected: 근데 래더지금 이 연락안옴 ${data}`);
-            if (playingGameInfo.player !== 'p1' && gameStart === false) {
-              socket.disconnect();
-              navigate('/game');
-            }
-          });
+
+        /*
+         * 이름 : playerDisconnected, data(디스커넥트 유저 상세정보)
+         * 특징 : 새로고침으로 한 유저의 소켓연결이 끊어지면, 발생합니다.
+         *       버튼으로 나가는경우엔, gameDestroyed가 처리합니다.
+         * 사용 : 방장의 연결해제 여부를 확인하고 남은 유저들을 정리합니다.
+         *       받아오는 데이터가 수정될 예정입니다.
+         */
+        socket.on('playerDisconnected', (data: any) => {
+          if (data.group === '1p') {
+            socketDisonnect(socket);
+            navigate('/game');
+          }
+        });
       });
     } else {
-      console.log('junselee: user정보가 없어지니까 날라가서 여기로 오는데');
-      console.log('junselee: 그냥 홈페이지 /game으로 보내버리는 상황');
-      if (socket) {
-        console.log('머야 소켓이있었어?');
-        socket.disconnect();
-      }
+      socketDisonnect(socket);
       navigate('/game');
     }
     return () => {
-      console.log('일로오긴해?: ' + roomid); // -1
-      if (socket) {
-        socket.off('playerDisconnected');
-        socket.off('gameDestroyed');
-        socket.off('gameStartCount');
-        socket.off('matchData');
-        socket.disconnect();
-      }
+      socketDisonnect(socket);
     };
   }, [user]);
-  // 페이지 이동말고, 특정컴포넌트를 보여주게 하는 방식으로 바꿔야 합니다.
-  // 함수형으로 반환하는 느낌으로 ..
   if (gameStart === false)
-    return (
-      <Background>
-        <GameRoomContainer>
-          <Header type={GAME} />
-          <GameRoomBody>
-            <GameArea>
-              <InfoArea>
-                <UserInfo>
-                  <PictureBlock>
-                    <ProfileImage
-                      src={info.avatarOne ? info.avatarOne : defaultProfile}
-                      size={150}
-                    />
-                  </PictureBlock>
-                  <span>{`${info.nicknameOne}`}</span>
-                  <span>{`Lv. ${info.ladderLevelOne}`}</span>
-                  <Bar>
-                    <Win id="win">{info.winCountOne}</Win>
-                    <Lose id="los">{info.loseCountOne}</Lose>
-                  </Bar>
-                </UserInfo>
-                <Count>{count}</Count>
-                <UserInfo>
-                  <PictureBlock>
-                    <ProfileImage
-                      src={info.avatarTwo ? info.avatarTwo : defaultProfile}
-                      size={150}
-                    />
-                  </PictureBlock>
-                  <span>{`${info.nicknameTwo}`}</span>
-                  <span>{`Lv. ${info.ladderLevelTwo}`}</span>
-                  <Bar>
-                    <Win id="win2">{info.winCountTwo}</Win>
-                    <Lose id="los">{info.loseCountTwo}</Lose>
-                  </Bar>
-                </UserInfo>
-              </InfoArea>
-              <Message>게임이 곧 시작됩니다</Message>
-            </GameArea>
-          </GameRoomBody>
-        </GameRoomContainer>
-      </Background>
-    );
+    if (!user) return <LoadingPage />;
+    else
+      return (
+        <Background>
+          <GameRoomContainer>
+            <Header type={GAME} />
+            <GameRoomBody>
+              <GameArea>
+                <InfoArea>
+                  <UserInfo>
+                    <PictureBlock>
+                      <ProfileImage
+                        src={info.avatarOne ? info.avatarOne : defaultProfile}
+                        size={150}
+                      />
+                    </PictureBlock>
+                    <span>{`${info.nicknameOne}`}</span>
+                    <span>{`Lv. ${info.ladderLevelOne}`}</span>
+                    <Bar>
+                      <Win id="win">{info.winCountOne}</Win>
+                      <Lose id="los">{info.loseCountOne}</Lose>
+                    </Bar>
+                  </UserInfo>
+                  <Count>{count ? count : 'vs'}</Count>
+                  <UserInfo>
+                    <PictureBlock>
+                      <ProfileImage
+                        src={info.avatarTwo ? info.avatarTwo : defaultProfile}
+                        size={150}
+                      />
+                    </PictureBlock>
+                    <span>{`${info.nicknameTwo}`}</span>
+                    <span>{`Lv. ${info.ladderLevelTwo}`}</span>
+                    <Bar>
+                      <Win id="win2">{info.winCountTwo}</Win>
+                      <Lose id="los">{info.loseCountTwo}</Lose>
+                    </Bar>
+                  </UserInfo>
+                </InfoArea>
+                <Message>게임이 곧 시작됩니다</Message>
+              </GameArea>
+            </GameRoomBody>
+          </GameRoomContainer>
+        </Background>
+      );
   else return <GameStart />; // RTData에서 넘겨받거나, 여기서 방정보 받은거중에, 모드만, 넘겨주거나 둘중하나임.
 };
 
 const PictureBlock = styled.div``;
-// css 애니메이션기능으로 추가.
 const Bar = styled.div`
   position: relative;
   width: 210px;
