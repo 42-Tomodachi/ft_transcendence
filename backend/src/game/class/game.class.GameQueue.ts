5e0256b878bebb97201df5318eb1dcdb3272f3bc
Jihoon Kim <kjhgomtang2@naver.com>
2022-09-15 02:05:31 +0900

    [BE] Fix: 래더 매칭 시 레벨 우선 매칭 구현
    - 매 초 마다 매칭 범위를 2배로 늘여가며 탐색

--- backend/src/game/class/game.class.GameQueue.ts
+++ backend/src/game/class/game.class.GameQueue.ts
@@ -6,12 +6,14 @@ class QueueInfo {
   player: Player;
   gentime: number;
   priority: number;
+  gapAllow: number;
   picked: boolean;
 
   constructor(player: Player) {
     this.player = player;
     this.gentime = new Date().getTime();
     this.priority = 0;
+    this.gapAllow = 5;
     this.picked = false;
   }
 }
@@ -74,13 +76,87 @@ export class GameQueue {
     console.log('switchMatchMaker: matching on');
   }
 
+  compareElements(one: QueueInfo, two: QueueInfo): boolean {
+    const oneLevel = one.player.user.getLadderLevel();
+    const oneMinLevel =
+      oneLevel - one.gapAllow < 0 ? 0 : oneLevel - one.gapAllow;
+    const oneMaxLevel = oneLevel + one.gapAllow;
+    const twoLevel = two.player.user.getLadderLevel();
+    const twoMinLevel =
+      twoLevel - two.gapAllow < 0 ? 0 : twoLevel - two.gapAllow;
+    const twoMaxLevel = twoLevel + two.gapAllow;
+
+    return oneMinLevel <= twoMaxLevel || oneMaxLevel >= twoMinLevel;
+  }
+
+  findMatchableOfOne(one: QueueInfo): QueueInfo {
+    const filteredQ = this.queue.filter((value) => {
+      if (value === one) return false;
+      return this.compareElements(one, value);
+    });
+    if (filteredQ.length === 0) return undefined;
+
+    filteredQ.sort((a, b) => {
+      return a.gentime - b.gentime;
+    });
+
+    return filteredQ.at(0);
+  }
+
   matchAlgorithm(players: Player[]): boolean {
-    const inittime = new Date().getTime();
+    const calltime = new Date().getTime();
+
+    this.queue.forEach((value) => {
+      switch (Math.floor((calltime - value.gentime) / 1000)) {
+        case 0:
+          break;
+        case 1:
+          value.priority = 1;
+          value.gapAllow = 5 * Math.pow(2, 1);
+          break;
+        case 2:
+          value.priority = 2;
+          value.gapAllow = 5 * Math.pow(2, 2);
+          break;
+        case 3:
+          value.priority = 3;
+          value.gapAllow = 5 * Math.pow(2, 3);
+          break;
+        case 4:
+          value.priority = 4;
+          value.gapAllow = 5 * Math.pow(2, 4);
+          break;
+        case 5:
+          value.priority = 5;
+          value.gapAllow = 5 * Math.pow(2, 5);
+          break;
+        case 6:
+          value.priority = 6;
+          value.gapAllow = 5 * Math.pow(2, 6);
+          break;
+        default:
+          value.priority = 7;
+          value.gapAllow = Number.POSITIVE_INFINITY;
+      }
+    });
+
     if (this.queue.length < 2) return false;
 
-    players.push(this.queue.shift().player);
-    players.push(this.queue.shift().player);
-    return true;
+    for (const element of this.queue) {
+      const opponent = this.findMatchableOfOne(element);
+      if (!opponent) continue;
+      players.push(element.player);
+      if (element.gentime <= opponent.gentime) {
+        players.push(opponent.player);
+      } else {
+        players.unshift(opponent.player);
+      }
+      this.queue.splice(this.queue.indexOf(element), 1);
+      this.queue.splice(this.queue.indexOf(opponent), 1);
+      this.switchMatchMaker();
+      return true;
+    }
+    return false;
   }
 
   pickMatchup(): void {
